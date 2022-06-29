45400dfc96481bc8797c777c2ccdafa9585d29e7
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2022-06-29 16:47:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    rect(30, 20, 55, 450, 11);
+    circle(200, 30, 55, 450);
+
+
+    
+}
+
+
+
